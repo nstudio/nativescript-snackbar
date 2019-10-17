4cde902cdb0b901cfff7f85750c486db0e386d47
Yasser Nascimento <yassern@protonmail.com>
2019-10-17 08:38:32 -0300

    init TTGSnackbar ios plugin

--- src/references.d.ts
+++ src/references.d.ts
@@ -1,4 +1,3 @@
 /// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
 /// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
 /// <reference path="./node_modules/tns-platform-declarations/android/androidx-28.d.ts" />
-/// <reference path="./typings/objc!SSSnackbar.d.ts" />
--- src/snackbar.ios.ts
+++ src/snackbar.ios.ts
@@ -1,104 +1,77 @@
 import { DismissReasons, SnackBarOptions } from './snackbar.common';
+import { Color } from 'tns-core-modules/color/color';
 export * from './snackbar.common';
 
+declare let TTGSnackbar;
+
 export class SnackBar {
-  private _snackbar = null;
-  private _isDismissedManual: boolean = false;
-
-  public simple(snackText: string) {
-    return new Promise((resolve, reject) => {
-      const timeout = 3;
-
-      try {
-        this._snackbar = SSSnackbar.snackbarWithMessageActionTextDurationActionBlockDismissalBlock(
-          snackText,
-          null,
-          timeout,
-          args => {
-            // Action, Do Nothing, just close it
-            this._snackbar.dismiss(); // Force close
-            resolve({
-              command: 'Dismiss',
-              reason: DismissReasons.MANUAL,
-              event: args
-            });
-          },
-          args => {
-            // Dismissal, Do Nothing
-            resolve({
-              command: 'Dismiss',
-              reason: DismissReasons.TIMEOUT,
-              event: args
-            });
-          }
-        );
-
-        this._snackbar.show();
-      } catch (ex) {
-        reject(ex);
-      }
+  public async simple(snackText: string) {
+    const snackbar = new TTGSnackbar({
+      message: snackText,
+      duration: 3
     });
+    snackbar.show();
+    return;
   }
 
-  public action(options: SnackBarOptions) {
-    return new Promise((resolve, reject) => {
-      try {
-        if (!options.hideDelay) options.hideDelay = 3000;
-
-        this._snackbar = SSSnackbar.snackbarWithMessageActionTextDurationActionBlockDismissalBlock(
-          options.snackText,
-          options.actionText,
-          options.hideDelay / 1000,
-          args => {
-            resolve({
-              command: 'Action',
-              event: args
-            });
-          },
-          args => {
-            const reason = this._isDismissedManual
-              ? DismissReasons.MANUAL
-              : DismissReasons.TIMEOUT;
-
-            this._isDismissedManual = false; // reset
-            resolve({
-              command: 'Dismiss',
-              reason: reason,
-              event: args
-            });
-          }
-        );
-
-        this._snackbar.show();
-      } catch (ex) {
-        reject(ex);
-      }
-    });
+  public async action(options: SnackBarOptions) {
+    return;
+    // try {
+    //   if (!options.hideDelay) options.hideDelay = 3000;
+    //   this._snackbar = SSSnackbar.snackbarWithMessageActionTextDurationActionBlockDismissalBlock(
+    //     options.snackText,
+    //     options.actionText,
+    //     options.hideDelay / 1000,
+    //     args => {
+    //       resolve({
+    //         command: 'Action',
+    //         event: args
+    //       });
+    //     },
+    //     args => {
+    //       const reason = this._isDismissedManual
+    //         ? DismissReasons.MANUAL
+    //         : DismissReasons.TIMEOUT;
+    //       this._isDismissedManual = false; // reset
+    //       resolve({
+    //         command: 'Dismiss',
+    //         reason: reason,
+    //         event: args
+    //       });
+    //     }
+    //   );
+    //   this._snackbar.show();
+    // } catch (ex) {
+    //   reject(ex);
+    // }
   }
 
-  public dismiss(options) {
-    return new Promise((resolve, reject) => {
-      if (this._snackbar !== null && this._snackbar !== 'undefined') {
-        try {
-          this._isDismissedManual = true;
-          this._snackbar.dismiss();
-
-          // Return AFTER the item is dismissed, 200ms delay
-          setTimeout(() => {
-            resolve({
-              action: 'Dismiss',
-              reason: DismissReasons.MANUAL
-            });
-          }, 200);
-        } catch (ex) {
-          reject(ex);
-        }
-      } else {
-        resolve({
-          action: 'None',
-          message: 'No actionbar to dismiss'
-        });
-      }
-    });
+  dismiss(): Promise<any> {
+    throw new Error('Method not implemented.');
   }
+
+  // public dismiss(options) {
+  //   return new Promise((resolve, reject) => {
+  //     if (this._snackbar !== null && this._snackbar !== 'undefined') {
+  //       try {
+  //         this._isDismissedManual = true;
+  //         this._snackbar.dismiss();
+  //         // Return AFTER the item is dismissed, 200ms delay
+  //         setTimeout(() => {
+  //           resolve({
+  //             action: 'Dismiss',
+  //             reason: DismissReasons.MANUAL
+  //           });
+  //         }, 200);
+  //       } catch (ex) {
+  //         reject(ex);
+  //       }
+  //     } else {
+  //       resolve({
+  //         action: 'None',
+  //         message: 'No actionbar to dismiss'
+  //       });
+  //     }
+  //   });
+  // }
 }
