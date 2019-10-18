88a00fc3b8f60611fc435c6554cd4145af8790f3
Brad Martin <bradwaynemartin@gmail.com>
2019-10-18 10:42:26 -0500

    fix test

--- demo/app/tests/tests.js
+++ demo/app/tests/tests.js
@@ -1,7 +1,7 @@
 describe('snackbar class', function() {
   it('can be instantiated', function() {
     var testSnackBar;
-    var snackbar = require('nativescript-snackbar');
+    var snackbar = require('@nstudio/nativescript-snackbar');
     if (snackbar) {
       testSnackBar = new snackbar.SnackBar();
       testSnackBar.simple('Simple Snackbar', 'red', '#fff', 3, false);
